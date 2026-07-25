085b321984cc79e5c35ba1295fed4c5a3f56b258
kylePellerin <kyl15003@esri.com>
2026-07-25 13:55:53 -0700

    idek bruh

--- KoiPonders/Incident.cs
+++ KoiPonders/Incident.cs
@@ -1,21 +1,77 @@
 ﻿using Esri.ArcGISRuntime.Geometry;
+using KoiPonders.Mvvm;
 
 namespace KoiPonders;
 
-public class Incident
+public class Incident : ObservableObject
 {
-    public string Classification { get; set; } = "";
-    public string PestName { get; set; } = "";
-    public string Severity { get; set; } = "LOW";
-    public string AffectedCrop { get; set; } = "";
-    public string Status { get; set; } = "OPEN";
-    public string Notes { get; set; } = "";
-    public string FieldName { get; set; } = "";
+    private string _classification = "";
+    private string _pestName = "";
+    private string _severity = "LOW";
+    private string _affectedCrop = "";
+    private string _status = "OPEN";
+    private string _notes = "";
+    private string _fieldName = "";
+    private bool _isExpanded;
+
+    public string Classification
+    {
+        get => _classification;
+        set => SetProperty(ref _classification, value);
+    }
+
+    public string PestName
+    {
+        get => _pestName;
+        set => SetProperty(ref _pestName, value);
+    }
+
+    public string Severity
+    {
+        get => _severity;
+        set
+        {
+            if (SetProperty(ref _severity, value))
+                OnPropertyChanged(nameof(SeverityColor));
+        }
+    }
+
+    public string AffectedCrop
+    {
+        get => _affectedCrop;
+        set => SetProperty(ref _affectedCrop, value);
+    }
+
+    public string Status
+    {
+        get => _status;
+        set => SetProperty(ref _status, value);
+    }
+
+    public string Notes
+    {
+        get => _notes;
+        set => SetProperty(ref _notes, value);
+    }
+
+    public string FieldName
+    {
+        get => _fieldName;
+        set => SetProperty(ref _fieldName, value);
+    }
+
     public double SpreadAcres { get; set; }
     public int Confidence { get; set; }
     public DateTime ReportDate { get; set; } = DateTime.Now;
     public MapPoint? Location { get; set; }
     public byte[]? Photo { get; set; }
+    public Guid ReportId { get; set; }
+
+    public bool IsExpanded
+    {
+        get => _isExpanded;
+        set => SetProperty(ref _isExpanded, value);
+    }
 
     public string DateDisplay => $"{ReportDate:MMM dd, yyyy}";
     public string SeverityColor => Severity switch
--- KoiPonders/Parcel.cs
+++ KoiPonders/Parcel.cs
@@ -1,13 +1,52 @@
 ﻿using Esri.ArcGISRuntime.Geometry;
+using KoiPonders.Mvvm;
 
 namespace KoiPonders;
 
-public class Parcel
+public class Parcel : ObservableObject
 {
-    public string Name { get; set; } = "";
-    public double Acres { get; set; }
-    public DateTime MappedDate { get; set; }
+    private string _name = "";
+    private double _acres;
+    private DateTime _mappedDate;
+    private bool _isExpanded;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (SetProperty(ref _name, value))
+                OnPropertyChanged(nameof(AcresDisplay));
+        }
+    }
+
+    public double Acres
+    {
+        get => _acres;
+        set
+        {
+            if (SetProperty(ref _acres, value))
+                OnPropertyChanged(nameof(AcresDisplay));
+        }
+    }
+
+    public DateTime MappedDate
+    {
+        get => _mappedDate;
+        set
+        {
+            if (SetProperty(ref _mappedDate, value))
+                OnPropertyChanged(nameof(MappedDisplay));
+        }
+    }
+
     public Geometry? Geometry { get; set; }
+
+    public bool IsExpanded
+    {
+        get => _isExpanded;
+        set => SetProperty(ref _isExpanded, value);
+    }
 
     public string AcresDisplay => $"{Acres:F1} Ac";
     public string MappedDisplay => $"Mapped {MappedDate:MMM dd, yyyy}";
