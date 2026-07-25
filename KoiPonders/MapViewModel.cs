03b514517239fdf1e3328cfbed9e56d9038034a1
Krishna Kant Mishra <kmishra@esri.com>
2026-07-24 19:16:10 -0700

    basic setup

--- KoiPonders/MapViewModel.cs
+++ KoiPonders/MapViewModel.cs
@@ -13,29 +13,23 @@ namespace KoiPonders
     {
         public MapViewModel()
         {
-            _map = new Map(SpatialReferences.WebMercator)
+            _map = new Map(BasemapStyle.ArcGISImagery)
             {
-                InitialViewpoint = new Viewpoint(new Envelope(-180, -85, 180, 85, SpatialReferences.Wgs84)),
-#warning To use ArcGIS location services (including basemaps) specify your API Key access token or require the user to sign in using a valid ArcGIS account.
-                //Basemap = new Basemap(BasemapStyle.ArcGISStreets)
+                InitialViewpoint = new Viewpoint(41.8902, -87.6298, 50000)
             };
         }
 
-        private Esri.ArcGISRuntime.Mapping.Map _map;
+        private Map _map;
 
         /// <summary>
         /// Gets or sets the map
         /// </summary>
-        public Esri.ArcGISRuntime.Mapping.Map Map
+        public Map Map
         {
             get => _map;
             set { _map = value; OnPropertyChanged(); }
         }
 
-        /// <summary>
-        /// Raises the <see cref="MapViewModel.PropertyChanged" /> event
-        /// </summary>
-        /// <param name="propertyName">The name of the property that has changed</param>
         protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
                  PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
--- KoiPonders/MauiProgram.cs
+++ KoiPonders/MauiProgram.cs
@@ -8,23 +8,6 @@ namespace KoiPonders
     {
         public static MauiApp CreateMauiApp()
         {
-            /* Authentication for ArcGIS location services:
-             * Use of ArcGIS location services, including basemaps and geocoding, requires either:
-             * 1) User authentication: Automatically generates a unique, short-lived access token when a user signs in to your application with their ArcGIS account
-             *    giving your application permission to access the content and location services authorized to an existing ArcGIS user's account.
-             *    You'll get an identity by signing into the ArcGIS Portal.
-             * 2) API key authentication: Uses a long-lived access token to authenticate requests to location services and private content.
-             *    Go to https://links.esri.com/create-an-api-key to learn how to create and manage an API key using API key credentials, and then call 
-             *    .UseApiKey("[Your ArcGIS location services API Key]")
-             *    in the UseArcGISRuntime call below. */
-
-            /* Licensing:
-             * Production deployment of applications built with the ArcGIS Maps SDK requires you to license ArcGIS functionality.
-             * For more information see https://links.esri.com/arcgis-runtime-license-and-deploy.
-             * You can set the license string by calling .UseLicense(licenseString) in the UseArcGISRuntime call below
-             * or retrieve a license dynamically after signing into a portal:
-             * ArcGISRuntimeEnvironment.SetLicense(await myArcGISPortal.GetLicenseInfoAsync()); */
-
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
@@ -34,19 +17,14 @@ namespace KoiPonders
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 })
                 .UseArcGISRuntime(config => config
-                // .UseLicense("[Your ArcGIS Maps SDK license string]")
-                // .UseApiKey("[Your ArcGIS location services API key]")
+                   .UseApiKey("yourkey")
                    .ConfigureAuthentication(auth => auth
-                       .UseDefaultChallengeHandler() // Use the default authentication dialog
+                       .UseDefaultChallengeHandler()
                        .UseCredentialPersistence()
-                  // .UseOAuthAuthorizeHandler(myOauthAuthorizationHandler) // Configure a custom OAuth dialog
-
-                  )
+                   )
                 );
-            // Enable support for TimestampOffset fields, which also changes behavior of Date fields.
-            // For more information see https://links.esri.com/DotNetDateTime
-            ArcGISRuntimeEnvironment.EnableTimestampOffsetSupport = true;
 
+            ArcGISRuntimeEnvironment.EnableTimestampOffsetSupport = true;
             return builder.Build();
         }
     }
